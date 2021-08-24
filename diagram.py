22dd78009d4c84a0e586063ae209de7734ad0509
Tomasz <raducha.tomasz@gmail.com>
2021-08-24 15:52:07 +0200

    diagram plot

--- diagram.py
+++ diagram.py
@@ -43,7 +43,11 @@ def plot():
 
     # plt.text(-0.95, 1.3, "prisoner's\ndilemma", fontsize=7.5)
     # plt.text(0.05, 1.3, "chicken\ngame", fontsize=7.5)
-    plt.text(-0.95, 0.55, "stag\nhunt", fontsize=11, alpha=0.7)
+    plt.text(-0.95, 0.55, "stag\nhunt", fontsize=11, alpha=0.65)
+    for i in range(1, 10):
+        plt.plot((i/-10.0, i/-10.0), (0, 1), color='black', alpha=0.08)
+        plt.plot((-1, 0), (i/10.0, i/10.0), color='black', alpha=0.08)
+
     # plt.text(0.05, 0.3, "harmony\ngame", fontsize=7.5)
     # plt.text(1.05, 1.6, "leader\ngame", fontsize=7.5)
     # plt.text(-0.8, -0.95, "    battle\nof sexes", fontsize=7.5)
@@ -52,7 +56,7 @@ def plot():
     plt.text(-2.6, -1.88, "strategy A is risk-dominant", fontsize=11, rotation=45, color=const.REDISH)
     plt.text(-2.9, -1.6, "strategy B is risk-dominant", fontsize=11, rotation=45, color=const.REDISH)
     # plt.text(-3.4, -0.9, "complex\ncoordination\ngame", fontsize=8, color=const.VIOLET)
-    plt.text(-3.4, 0.55, "general coordination\ngame area", fontsize=11, color=const.GREEN_DARK)
+    plt.text(-3.4, 0.55, "general coordination\ngame area", fontsize=11, color=const.GREEN_DARKER)
 
     # plt.figtext(0.02, 0.02, "R=1\nP=0", fontsize=9)
 
