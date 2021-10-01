8ac1681572b90af2bb88c18f75e30f52b0b6ca6d
tomaszraducha <raducha.tomasz@gmail.com>
2021-10-01 19:34:08 +0200

    analtics

--- analytics.py
+++ analytics.py
@@ -14,6 +14,31 @@ def system(x, d_t, config):
 
     return d_x * d_t
 
+
+def nowakIM(x, d_t, config):
+    k = 32
+    S = config['S']
+    T = config['T']
+
+    c = (k+3.0 + 3.0*(S-T)) / ((k+3.0)*(k-2.0))
+
+    d_x = (S+T-1.0) * x**3.0 + (1.0-2*S-T-c) * x**2.0 + (S+c) * x
+
+    return d_x * d_t
+
+
+def nowakBD(x, d_t, config):
+    k = 3
+    S = config['S']
+    T = config['T']
+
+    c = (1.0+S-T) / (k-2.0)
+
+    d_x = (S+T-1.0) * x**3.0 + (1.0-2*S-T-c) * x**2.0 + (S+c) * x
+
+    return d_x * d_t
+
+
 def system2(x, d_t, config):
     """
     for UI and BR mean field, only for R=1, P=0
@@ -33,12 +58,14 @@ def system2(x, d_t, config):
 
 
 def find_steady(config=None):
+    main = nowakIM
+
     d_x = 1.0
     d_t = 0.01
     x = 1.0/2.0
     i = 0
     while np.abs(d_x) > 1e-06:
-        d_x = system(x, d_t, config=config)
+        d_x = main(x, d_t, config=config)
         x = min(max(x + d_x, 0.0), 1.0)
         i += 1
         if i > 100000:
@@ -50,25 +77,25 @@ def find_steady(config=None):
 def plot_repicator():
     conf = {"R": 1, "P": 0, "S": None, "T": None}
     res = []
-    for T in np.linspace(-3, 3, 30):
+    for T in np.linspace(-2, 1, 30):
         conf['T'] = T
         res_in = []
-        for S in np.linspace(-3, 10, 30):
+        for S in np.linspace(-3, 0, 30):
             conf['S'] = S
             res_in.append(find_steady(config=conf))
         res.append(res_in)
 
     fig = plt.figure(figsize=(4, 3.5))
-    im = plt.imshow(res, cmap='jet', origin='lower', extent=[-3, 10, -3, 3], interpolation='none')
+    im = plt.imshow(res, cmap='jet', origin='lower', extent=[-3, 0, -2, 1], interpolation='none')
     cbar = fig.colorbar(im, fraction=0.0467, pad=0.04)
     cbar.ax.tick_params()
     cbar.set_clim(0, 1)
 
     plt.title('Mean field')
-    plt.axvline(0, linestyle='--')
-    # plt.axvline(-2, linestyle='--')
-    plt.axhline(1, linestyle='--')
-    # plt.axhline(0, linestyle='--')
+    plt.axvline(-1, linestyle='--')
+    plt.axvline(-2, linestyle='--')
+    plt.axhline(-1, linestyle='--')
+    plt.axhline(0, linestyle='--')
 
     plt.xlabel('S')
     plt.ylabel('T')
